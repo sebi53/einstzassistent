71e6a63e1add7f484a795079481431fd0ef64ca6
sebi53 <sebi5353@gmail.com>
2026-07-24 01:13:13 +0000

    v6.5.2: Allianz Abschnitt 7 - Notrufzentrale/Besondere-Probleme Koordinaten korrigiert

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Einsatz-Assistent – Service Worker (Offline-Cache)
-const CACHE = 'einsatz-assistent-v54';
-const APP_VERSION='6.5.1';
+const CACHE = 'einsatz-assistent-v55';
+const APP_VERSION='6.5.2';
 const ASSETS = [
   './',
   './index.html',
